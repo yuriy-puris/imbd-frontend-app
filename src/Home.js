b44bf6bfd4c83f0955df88b044e79930ab4807f0
Yuriy Puris <ypuris@itera-research.com>
2020-03-16 16:46:34 +0200

    add fetch hook

--- src/Home.js
+++ src/Home.js
@@ -1,45 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { API_URL, API_KEY, API_BASE_URL, POSTER_SIZE, BAKDROP_SIZE } from './config';
 
+// components
 import HeroImage from './components/sharedComponents/HeroImage';
 import Grid from './components/sharedComponents/Grid';
 import SearchBar from './components/sharedComponents/SearchBar';
 
+// custom hooks
+import { useHomeFetch } from './components/hooks/useHomeFetch';
 
 const Home = () => {
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
   
-  const [state, setState] = useState({ movies: []  });
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  console.log(state);
-
-  const fetchMovies = async endpoint => {
-    setError(false);
-    setLoading(true);
-
-    try {
-      const result = await (await fetch(endpoint)).json();
-      setState(prev => ({
-        ...prev,
-        movies: [...result.results],
-        heroImage: prev.heroImage || result.results[0],
-        currentPage: result.page,
-        totalPages: result.total_pages
-      }));
-      
-    } catch (error) {
-      setError(true);
-      console.log(error);
-    }
-
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`)
-  }, [])
-
   return (
     <>
       <HeroImage />
